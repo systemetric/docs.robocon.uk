9747f6aeb0a3caf792e29fda405d900651a6a4cd
Oli <147245351+what-does-that-do@users.noreply.github.com>
2024-11-30 13:20:35 +0000

    Added Tailwind css support, added nicer fonts

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Hosted on the subdomain https://docs.robocon.uk
 
 Built with the Starlight docs framework, on Astro. Tailwind is installed.
+
+If you are wondering how something works, look at the Starlight docs first before you look at the Astro ones - the functionality is slightly different!
+
+Fonts are installed via Fontsource.
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,38 +2,50 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'RoboCon Docs',
-			social: {
-				instagram: 'https://www.instagram.com/hillsroadrobocon/',
-				twitter: 'https://twitter.com/HRobocon/',
-				facebook: 'https://www.facebook.com/HRobocon/'
-			},
-			sidebar: [
-				{
-					label: 'Getting Started',
-					autogenerate: { directory: 'getting-started' },
-				},
-				{
-					label: 'Hardware',
-					autogenerate: { directory: 'hardware' },
-				},
-				{
-					label: 'Programming',
-					autogenerate: { directory: 'programming' },
-				},
-				{
-					label: 'Tools',
-					autogenerate: { directory: 'tools' },
-				},
-				{
-					label: 'Tutorials',
-					autogenerate: { directory: 'tutorials' },
-				},
-			],
+    integrations: [starlight({
+        title: 'RoboCon Docs',
+        social: {
+            instagram: 'https://www.instagram.com/hillsroadrobocon/',
+            twitter: 'https://twitter.com/HRobocon/',
+            facebook: 'https://www.facebook.com/HRobocon/'
+        },
+        sidebar: [
+            {
+                label: 'Getting Started',
+                autogenerate: { directory: 'getting-started' },
+            },
+            {
+                label: 'Hardware',
+                autogenerate: { directory: 'hardware' },
+            },
+            {
+                label: 'Programming',
+                autogenerate: { directory: 'programming' },
+            },
+            {
+                label: 'Tools',
+                autogenerate: { directory: 'tools' },
+            },
+            {
+                label: 'Tutorials',
+                autogenerate: { directory: 'tutorials' },
+            },
+        ],
+        customCss: [
+                // Fonts
+                '@fontsource/space-mono/400.css',
+                '@fontsource/rubik-mono-one/400.css',
+                '@fontsource/lexend/400.css',
+                // Path to your Tailwind base styles:
+                './src/styling.css',
+            ],
 		}),
-	],
+        tailwind({
+                // Disable the default base styles:
+                applyBaseStyles: false,
+        })],
 });
